b622b44acf8d54fb834d0ad405a93e10952e15a2
RafaGiovanelli <rsgiovanelli98@hotmail.com>
2024-04-20 20:37:52 -0300

    Feat: Conclusao da task 06

--- exercises/task_06/algorithm.js
+++ exercises/task_06/algorithm.js
@@ -1,15 +1,25 @@
 //! Exercicio 6
 //* Faça um algoritimo que gere um número aleatorio de 0 a 10 e leia um número de 0 10, e diga se é o número que foi pensado ou não.
 
-
 //* Validações importantes!
 //* Validar se os valores são númericos.
 
 //? Se for igual ao número lido deverá retornar a seguinte mensagem: Número gerado "X", Você adivinhou o número que estou pensando;
 //? Se for diferente disso o console deve apresentar:  Número gerado "X" você errou o número que estou pensando.
 
-const task06 = () => {
-  //* Escreva sua solução aqui
+const task06 = (numero) => {
+  numero = Number(numero);
+
+  if (isNaN(numero)) {
+    throw new Error("Caracteres invalidos! Inserir apenas numeros.");
+  }
+  
+  const numeroAleatorio = Math.floor(Math.random() * 10);
+
+  if (numeroAleatorio === numero) {
+    return `Número gerado "${numeroAleatorio}", Você adivinhou o número que estou pensando!`
+  }
+  return `Número gerado "${numeroAleatorio}" você errou o número que estou pensando!`
 };
 
-task06();
+console.log(task06(process.argv[2]));
