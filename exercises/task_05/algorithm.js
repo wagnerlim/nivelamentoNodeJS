c35c64d2f8c6f67a3f155713ad10d85fb7bb94dc
RafaGiovanelli <rsgiovanelli98@hotmail.com>
2024-04-20 20:15:49 -0300

    Feat: Conclusão da task 05

--- exercises/task_05/algorithm.js
+++ exercises/task_05/algorithm.js
@@ -1,5 +1,5 @@
 //! Exercicio 5
-//* Crie um algoritimo que leia um número qualquer e verifique se ele é par ou impar 
+//* Crie um algoritimo que leia um número qualquer e verifique se ele é par ou impar
 
 //* Validações importantes!
 //* Validar se os valores são númericos.
@@ -9,8 +9,17 @@
 //? Ex: 8
 //? Resp: O número 8 é Impar!
 
-const task05 = () => {
-  //* Escreva sua solução aqui
+const task05 = (numero) => {
+  numero = Number(numero);
+
+  if (isNaN(numero)) {
+    throw new Error("Caracteres invalidos! Inserir apenas numeros.")
+  }
+
+  if (numero % 2 == 0) {
+    return `O número ${numero} é Par`;
+  } 
+  return `O número ${numero} é Impar`;
 };
 
-task05();
+console.log(task05(process.argv[2], process.argv[3]));
