190221c02bd36d68e9277976b74b0a4f205591ee
wagnerlim <wagnerlima0910@gmail.com>
2024-04-20 21:43:26 -0300

    feat: sample arrays

--- basic/values.js
+++ basic/values.js
@@ -1,12 +1,24 @@
-import { readFile } from "fs/promises";
-
-async function getJsonData() {
-  const data = await readFile("./basic/values.json");
-  return JSON.parse(data);
-}
-
 async function sampleString() {
-  const data = await getJsonData();
+  for (let index = 0; index <= 10; index++) {
+    // console.log("index :>> ", index);
+  }
+  const arrayDeValores = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+  // arrayDeValores.forEach((element) => {
+  // });
+  // let valor = 0;
+  // do {
+  //   console.log("valor :>> ", valor);
+  //   valor++;
+  // } while (valor !== 5);
+
+  // while (valor !== 5) {
+  //   valor++;
+  //   console.log("valor :>> ", valor);
+  // }
+  arrayDeValores.push(11);
+  console.log("arrayDeValores :>> ", arrayDeValores);
+  arrayDeValores.pop();
+  console.log("arrayDeValores :>> ", arrayDeValores);
 }
 
 sampleString();
